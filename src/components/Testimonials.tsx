59f587551a934bdcf748a6b7cc9af7d2e37af162
sushmasri1906 <sushmasri1906@gmail.com>
2025-03-15 12:41:27 +0530

    testimonials updated

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { FaUserCircle, FaStar, FaRegStar } from "react-icons/fa";
+import { FaStar, FaRegStar } from "react-icons/fa";
 
 const testimonials = [
 	{
